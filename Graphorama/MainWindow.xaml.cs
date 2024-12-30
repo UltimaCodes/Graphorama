368ff7b7ecbf12cf72ec75b2b8e82aca59be3038
UltimaCodes <ryaanaqil@gmail.com>
2024-12-30 18:23:58 +0500

    init

---
+++ Graphorama/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+using System.Windows.Controls;
+using System.Data;
+using OxyPlot.Wpf;
+
+namespace Graphorama
+{
+    public partial class MainWindow : Window
+    {
+        private PlotModel plotModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGraph();
+        }
+
+        private void InitializeGraph()
+        {
+            plotModel = new PlotModel { Title = "Graphorama" };
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "X-Axis" });
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Y-Axis" });
+            PlotView.Model = plotModel;
+        }
+
+        private void PlotFunction(string equation)
+        {
+            var series = new LineSeries { Title = equation };
+            try
+            {
+                for (double x = -10; x <= 10; x += 0.1)
+                {
+                    double y = EvaluateEquation(equation, x);
+                    series.Points.Add(new DataPoint(x, y));
+                }
+                plotModel.Series.Add(series);
+                plotModel.InvalidatePlot(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error plotting function: {ex.Message}");
+            }
+        }
+
+        private double EvaluateEquation(string equation, double x)
+        {
+            equation = equation.Replace("x", x.ToString());
+            DataTable table = new DataTable();
+            return Convert.ToDouble(table.Compute(equation, ""));
+        }
+
+        private void OnPlotButtonClick(object sender, RoutedEventArgs e)
+        {
+            var equation = EquationInput.Text;
+            PlotFunction(equation);
+        }
+
+        private void OnClearButtonClick(object sender, RoutedEventArgs e)
+        {
+            plotModel.Series.Clear();
+            plotModel.InvalidatePlot(true);
+        }
+
+        private void OnZoomResetClick(object sender, RoutedEventArgs e)
+        {
+            plotModel.Axes[0].Reset();
+            plotModel.Axes[1].Reset();
+            plotModel.InvalidatePlot(true);
+        }
+    }
+}
